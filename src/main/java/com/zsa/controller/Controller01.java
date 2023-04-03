7c8540b685938e4e65d996b9f41b9f8c06330daa
zhangshanao <2715407070@qq.com>
2023-04-03 10:13:54 +0800

    test分支进行了测试并且输出了一条语句

--- src/main/java/com/zsa/controller/Controller01.java
+++ src/main/java/com/zsa/controller/Controller01.java
@@ -9,6 +9,7 @@ public class Controller01 {
     @GetMapping()
     public String get(@PathVariable String age,String pass){
         System.out.println("hello git");
+        System.out.println("test分支进行测试");
         return "hello world";
     }
 }
